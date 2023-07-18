4dd2f1db5cef3f49366fa67b60dc21b768d21752
PC <PC@LAPTOP-ACP43AFB>
2023-07-18 10:07:17 +0800

    添加项目文件。

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(317, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "显示数据";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "是";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(687, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "否";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 384);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(724, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "是/否开机自启动？";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(38, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(724, 274);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Microsoft.Win32;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private string connStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=MAIN.mdb;jet oledb:database password=LW;Data Source=C:\Program Files (x86)\巡检管理系统A1.0+\MAIN.MDB";//名称；密码；地址
+
+        private OleDbConnection conn = null;
+        private OleDbDataAdapter adapter = null;
+        private DataTable dt = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 连接数据库并显示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 连接数据库，需要传递连接字符串
+            conn = new OleDbConnection(connStr);
+            // 打开数据库连接
+            conn.Open();
+
+            // "Select * from .."为SQL语句，意思是从数据库中选择叫做“lwnhdd”的表，“conn”为连接
+            adapter = new OleDbDataAdapter("Select * from lwnhdd", conn);
+
+            label1.Text = "lwnhdd";
+            // CommandBuilder对应的是数据适配器，需要传递参数
+            var cmd = new OleDbCommandBuilder(adapter);
+
+            // 在内存中创建一个DataTable，用来存放、修改数据库表
+            dt = new DataTable();
+            // 通过适配器把表的数据填充到内存dt
+            adapter.Fill(dt);
+            // 把数据显示到界面
+            dataGridView1.DataSource = dt.DefaultView;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "lwnhdd";
+        }
+        private void FormDatabase_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>  
+        /// 修改程序在注册表中的键值  
+        /// </summary>  
+        /// <param name="isAuto">true:开机启动,false:不开机自启</param> 
+        private void AutoStart(bool isAuto, bool showinfo)
+        {
+            try
+            {
+                if (isAuto == true)
+                {
+                    RegistryKey R_local = Registry.CurrentUser;//RegistryKey R_local = Registry.CurrentUser;
+                    RegistryKey R_run = R_local.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                    R_run.SetValue("WindowsFormsApp", Application.ExecutablePath);
+                    R_run.Close();
+                    R_local.Close();
+                }
+                else if (isAuto == false)
+                {
+                    RegistryKey R_local = Registry.CurrentUser;//RegistryKey R_local = Registry.CurrentUser;
+                    RegistryKey R_run = R_local.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+                    R_run.DeleteValue("WindowsFormsApp", false);
+                    R_run.Close();
+                    R_local.Close();
+                }
+            }
+            catch (Exception)
+            {
+                if (showinfo)
+                    MessageBox.Show("您需要管理员权限修改", "提示");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AutoStart(true, true);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AutoStart(false, true);
+        }
+    }
+}
